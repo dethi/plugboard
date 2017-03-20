738d4927b988c65990bf81686949ffbb396c166d
Alexandre Chabrolin <chabro_a@epita.fr>
2017-03-20 20:28:04 +0100

    login: add login and sign in

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                    <img src={{$gravatar_url}} />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
