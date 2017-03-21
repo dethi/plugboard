5a76648891f040ec328895711c5ae65e3db6eab7
Alexandre Chabrolin <chabro_a@epita.fr>
2017-03-21 01:12:13 +0100

    login: remove relevant comments

--- app/Http/Controllers/GravatarController.php
+++ app/Http/Controllers/GravatarController.php
@@ -13,8 +13,6 @@ class GravatarController extends Controller
   * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
   * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
   * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
-  * @param boole $img True to return a complete IMG tag False for just the URL
-  * @param array $atts Optional, additional key/value attributes to include in the IMG tag
   * @return String containing either just a URL or a complete image tag
   * @source https://gravatar.com/site/implement/images/php/
   */
