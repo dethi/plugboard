3d0fa1aa78bf00f2369833fded24c007a92b71f0
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-03-22 16:34:08 +0100

    js: optimize commons chunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var assetsPath = path.resolve(__dirname, 'resources/assets');
 
 module.exports = {
-  entry: path.join(assetsPath, 'js/index.js'),
+  entry: {
+    app: path.join(assetsPath, 'js/index.js'),
+  },
   output: {
-    filename: 'app.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'public/js')
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        // only package from `node_modules` directory
+        return module.context && module.context.indexOf('node_modules') !== 1;
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    })
+  ]
 }
