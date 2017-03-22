fb7cb20ecbf62017c2757fa4cd1896d11b8c7601
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-03-22 23:58:37 +0100

    js: configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/, exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader']
+      }
     ]
   },
   plugins: [
