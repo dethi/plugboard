856f0b6eb0fe603728b2837a5574b678d27a29a4
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-03-27 14:31:38 +0200

    view: design register component

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,114 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
 @section('content')
-<div>Register</div>
-<div>
-  <form role="form" method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
+<section class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <div class="card is-fullwidth">
+          <header class="card-header">
+            <p class="card-header-title">Register</p>
+          </header>
 
-    <div>
-      <label for="name">Name</label>
+          <div class="card-content">
+            <div class="content">
+              <form class="control" role="form" method="POST" action="{{ url('/register') }}">
+                {{ csrf_field() }}
 
-      <div>
-        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+                <div class="field">
+                  <div class="control has-icon">
+                    <input class="input {{ $errors->has('name') ? 'is-danger' : '' }}"
+                      name="name"
+                      value="{{ old('name') }}"
+                      type="text"
+                      placeholder="Name"
+                      required autofocus
+                    />
 
-        @if ($errors->has('name'))
-        <span>
-          <strong>{{ $errors->first('name') }}</strong>
-        </span>
-        @endif
+                    <span class="icon is-small">
+                      <i class="fa fa-user"></i>
+                    </span>
+
+                    @if ($errors->has('name'))
+                      <span class="help is-danger">{{ $errors->first('name') }}</span>
+                    @endif
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="control has-icon">
+                    <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
+                      name="email"
+                      value="{{ old('email') }}"
+                      type="email"
+                      placeholder="Email"
+                      required
+                    />
+
+                    <span class="icon is-small">
+                      <i class="fa fa-envelope"></i>
+                    </span>
+
+                    @if ($errors->has('email'))
+                      <span class="help is-danger">{{ $errors->first('email') }}</span>
+                    @endif
+                  </div>
+                </div>
+
+                <?php
+                $password_error = $errors->hasAny(['password', 'password_confirmation'])
+                  ? $errors->first('password') ?? $errors->first('password_confirmation')
+                  : ''
+                  ;
+                ?>
+
+                <div class="field">
+                  <div class="control has-icon">
+                    <input
+                      class="input {{ $password_error ? 'is-danger' : '' }}"
+                      name="password"
+                      type="password"
+                      placeholder="Password"
+                      required
+                    />
+
+                    <span class="icon is-small">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="control has-icon">
+                    <input
+                      class="input {{ $password_error ? 'is-danger' : '' }}"
+                      name="password_confirmation"
+                      type="password"
+                      placeholder="Confirm Password"
+                      required
+                    />
+
+                    <span class="icon is-small">
+                      <i class="fa fa-lock"></i>
+                    </span>
+
+                    @if ($password_error)
+                      <span class="help is-danger">
+                        {{ $password_error }}
+                      </span>
+                    @endif
+                  </div>
+                </div>
+
+                <div class="control">
+                  <button class="button is-primary is-fullwidth uppercase" type="submit">Register</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div>
-      <label for="email">E-Mail Address</label>
-
-      <div>
-        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-
-        @if ($errors->has('email'))
-        <span>
-          <strong>{{ $errors->first('email') }}</strong>
-        </span>
-        @endif
-      </div>
-    </div>
-
-    <div>
-      <label for="password">Password</label>
-
-      <div>
-        <input id="password" type="password" name="password" required>
-
-        @if ($errors->has('password'))
-        <span>
-          <strong>{{ $errors->first('password') }}</strong>
-        </span>
-        @endif
-      </div>
-    </div>
-
-    <div>
-      <label for="password-confirm">Confirm Password</label>
-
-      <div>
-        <input id="password-confirm" type="password" name="password_confirmation" required>
-      </div>
-    </div>
-
-    <div>
-      <button type="submit">
-        Register
-      </button>
-    </div>
-  </form>
-
-  @endsection
+  </div>
+</section>
+@endsection
