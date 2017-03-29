12d2984c414cf648383a5ece1c09b4a8f34aa30f
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-03-30 00:23:49 +0200

    fix: unshallow git clone on travis during deploy
    Travis CI only clone with a depth of 50 commits. However
    it's not allow to push to a remote from a shallow clone.
    The fix convert the clone to a full clone before each
    deployment.

--- scripts/deploy-app-plugboard_prod.sh
+++ scripts/deploy-app-plugboard_prod.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 if [ $TRAVIS_BRANCH == 'master' ]; then
     echo '=== Starting deployment of plugboard_prod...'
 
+    git fetch --unshallow
+
     git remote add deploy 'deploy@plugboard.xyz:~/plugboard_prod'
     ssh-agent bash -c 'ssh-add "$DEPLOY_KEY" && git push --force deploy master:master'
 
