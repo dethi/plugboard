f78066c314f2f3a3eec11d87b315c11be4b88fd9
Alexandre Chabrolin <chabro_a@epita.fr>
2017-04-08 13:56:04 +0200

    password-reset: update views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,56 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-4 is-offset-4">
+                    <div class="card is-fullwidth">
+                        <header class="card-header">
+                            <p class="card-header-title">Reset Password</p>
+                        </header>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                        @if (session('status'))
+                            <div class="notification is-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="card-content">
+                            <div class="content">
+                                <form class="control" role="form" method="POST" action="{{ route('password.email') }}">
+                                    {{ csrf_field() }}
+                                    <div class="field">
+                                        <div class="control has-icon">
+                                            <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
+                                                   name="email"
+                                                   value="{{ old('email') }}"
+                                                   type="email"
+                                                   placeholder="Email"
+                                                   required autofocus
+                                            />
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                            <span class="icon is-small">
+                      <i class="fa fa-envelope"></i>
+                    </span>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                            @if ($errors->has('email'))
+                                                <span class="help is-danger">{{ $errors->first('email') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="field">
+                                        <div class="control">
+                                            <button class="button is-primary is-fullwidth uppercase" type="submit">Send Password Reset Link</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
