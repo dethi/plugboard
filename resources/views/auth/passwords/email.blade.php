1120b57e5bcd1590e9fbd7f343b10e4af2e2e2db
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-04-17 15:02:03 +0200

    misc: convert indent to 2 spaces

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,56 +1,56 @@
 @extends('layouts.base')
 
 @section('content')
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-4 is-offset-4">
-                    <div class="card is-fullwidth">
-                        <header class="card-header">
-                            <p class="card-header-title">Reset Password</p>
-                        </header>
+<section class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <div class="card is-fullwidth">
+          <header class="card-header">
+            <p class="card-header-title">Reset Password</p>
+          </header>
 
-                        @if (session('status'))
-                            <div class="notification is-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+          @if (session('status'))
+            <div class="notification is-success">
+              {{ session('status') }}
+            </div>
+          @endif
 
-                        <div class="card-content">
-                            <div class="content">
-                                <form class="control" role="form" method="POST" action="{{ route('password.email') }}">
-                                    {{ csrf_field() }}
-                                    <div class="field">
-                                        <div class="control has-icon">
-                                            <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
-                                                   name="email"
-                                                   value="{{ old('email') }}"
-                                                   type="email"
-                                                   placeholder="Email"
-                                                   required autofocus
-                                            />
+          <div class="card-content">
+            <div class="content">
+              <form class="control" role="form" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
+                <div class="field">
+                  <div class="control has-icon">
+                    <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
+                          name="email"
+                          value="{{ $email or old('email') }}"
+                          type="email"
+                          placeholder="Email"
+                          required autofocus
+                    />
 
-                                            <span class="icon is-small">
+                    <span class="icon is-small">
                       <i class="fa fa-envelope"></i>
                     </span>
 
-                                            @if ($errors->has('email'))
-                                                <span class="help is-danger">{{ $errors->first('email') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                    <div class="field">
-                                        <div class="control">
-                                            <button class="button is-primary is-fullwidth uppercase" type="submit">Send Password Reset Link</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                    @if ($errors->has('email'))
+                      <span class="help is-danger">{{ $errors->first('email') }}</span>
+                    @endif
+                  </div>
                 </div>
+
+                <div class="field">
+                  <div class="control">
+                    <button class="button is-primary is-fullwidth uppercase" type="submit">Send Password Reset Link</button>
+                  </div>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
