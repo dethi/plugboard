9eec22e7e58b890d15a32c25c1ef9d08a607d059
Antoine C <antoine.cormerais@epita.fr>
2017-04-27 01:47:39 +0200

    palette: add more gates and factorize code to deal with multiple types of gate.

--- resources/assets/components/Palette.js
+++ resources/assets/components/Palette.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-import { ElementBlueprint } from '../libs/element/elementBlueprint';
+import { GateType, ElementBlueprint } from '../libs/element/elementBlueprint';
+
+// Used to create html for a single element in the palette
+class SelectableElement extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="componentPalette">
+        <a onClick={this.props.onClick}>
+          <figure className="image is-64x64">
+            <img src={this.props.img} id="test-img" />
+          </figure>
+        </a>
+      </div>
+    );
+  }
+}
 
 export default class Palette extends Component {
   constructor(props) {
@@ -11,26 +29,29 @@ export default class Palette extends Component {
       elements: [
         ElementBlueprint.createInputBlueprint(),
         ElementBlueprint.createOutputBlueprint(),
-        ElementBlueprint.createDefaultGateBlueprint(),
-        ElementBlueprint.createOrGateBlueprint()
+        ElementBlueprint.createGateBlueprint('not', GateType.NOT, 1),
+        ElementBlueprint.createGateBlueprint('and', GateType.AND),
+        ElementBlueprint.createGateBlueprint('nand', GateType.NAND),
+        ElementBlueprint.createGateBlueprint('or', GateType.OR),
+        ElementBlueprint.createGateBlueprint('nor', GateType.NOR),
+        ElementBlueprint.createGateBlueprint('nxor', GateType.NXOR)
       ],
       curElementId: 0
     };
   }
-
+  //Not working yet
   updateStateOnClick(i) {
-    console.log('upppdae');
     this.setState({ curElementId: i });
     this.props.updatePalette(this.state.elements[i]);
   }
   currentSelected(i) {
-    console.log('called');
     if (i == this.props.state.curElementId) {
       ('selectedComponent');
     } else {
       return '';
     }
   }
+
   render() {
     const stylePanel = {
       float: 'left',
@@ -50,47 +71,51 @@ export default class Palette extends Component {
 
           <TabPanel>
             <div className="box">
+
               <h3 className="title is-3 has-text-centered">Input/Ouput</h3>
               <div className="componentsPalette">
-                <div className="componentPalette">
-                  <a
-                    className={this.currentSelected.bind(this, 0)}
-                    onClick={this.updateStateOnClick.bind(this, 0)}
-                  >
-                    <figure className=" image is-64x64">
-                      <img src={this.state.elements[0].img} id="test-img" />
-                    </figure>
-                  </a>
-                </div>
-
-                <div className="componentPalette">
-                  <a onClick={this.updateStateOnClick.bind(this, 1)}>
-                    <figure className="image is-64x64">
-                      <img src={this.state.elements[1].img} />
-                    </figure>
-                  </a>
-                </div>
+                <SelectableElement
+                  img={this.state.elements[0].img}
+                  onClick={this.updateStateOnClick.bind(this, 0)}
+                />
+                <SelectableElement
+                  img={this.state.elements[1].img}
+                  onClick={this.updateStateOnClick.bind(this, 1)}
+                />
               </div>
             </div>
-
+            <div className="box">
+              <h3 className="title is-3 has-text-centered">Gate 1/1</h3>
+              <div className="componentsPalette">
+                <SelectableElement
+                  img={this.state.elements[2].img}
+                  onClick={this.updateStateOnClick.bind(this, 2)}
+                />
+              </div>
+            </div>
             <div className="box">
               <h3 className="title is-3 has-text-centered">Gate 2/1</h3>
-
               <div className="componentsPalette">
-                <div className="componentPalette">
-                  <a onClick={this.updateStateOnClick.bind(this, 2)}>
-                    <figure className="image is-64x64">
-                      <img src={this.state.elements[2].img} />
-                    </figure>
-                  </a>
-                </div>
-                <div className="componentPalette">
-                  <a onClick={this.updateStateOnClick.bind(this, 3)}>
-                    <figure className="image is-64x64">
-                      <img src={this.state.elements[3].img} />
-                    </figure>
-                  </a>
-                </div>
+                <SelectableElement
+                  img={this.state.elements[3].img}
+                  onClick={this.updateStateOnClick.bind(this, 3)}
+                />
+                <SelectableElement
+                  img={this.state.elements[4].img}
+                  onClick={this.updateStateOnClick.bind(this, 4)}
+                />
+                <SelectableElement
+                  img={this.state.elements[5].img}
+                  onClick={this.updateStateOnClick.bind(this, 5)}
+                />
+                <SelectableElement
+                  img={this.state.elements[6].img}
+                  onClick={this.updateStateOnClick.bind(this, 6)}
+                />
+                <SelectableElement
+                  img={this.state.elements[7].img}
+                  onClick={this.updateStateOnClick.bind(this, 7)}
+                />
               </div>
             </div>
           </TabPanel>
