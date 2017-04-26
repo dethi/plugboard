833234da3627126c7e723c91fe8b192448f06669
Antoine C <antoine.cormerais@epita.fr>
2017-04-26 18:27:39 +0200

    add img components represent component with their img in palette design the palette with bulma and flexbox

--- resources/assets/libs/element/elementBlueprint.js
+++ resources/assets/libs/element/elementBlueprint.js
@@ -5,24 +5,72 @@ const ElementType = {
 };
 
 class ElementBlueprint {
-  constructor(elementType, nbInput, nbOutput, colorOn, colorOff) {
+  constructor(
+    name,
+    elementType,
+    nbInput,
+    nbOutput,
+    colorOn,
+    colorOff,
+    img,
+    truthTable
+  ) {
+    this.name = name;
     this.elementType = elementType;
     this.colorOn = colorOn;
     this.colorOff = colorOff;
     this.nbInput = nbInput;
     this.nbOutput = nbOutput;
+    this.img = img;
+    this.truthTable = truthTable;
   }
 
   static createInputBlueprint() {
-    return new ElementBlueprint(ElementType.INPUT, 0, 1, 'green', 'red');
+    return new ElementBlueprint(
+      'input',
+      ElementType.INPUT,
+      0,
+      1,
+      'green',
+      'red',
+      '/static/components/input.png'
+    );
   }
 
   static createOutputBlueprint() {
-    return new ElementBlueprint(ElementType.OUTPUT, 1, 0, 'green', 'red');
+    return new ElementBlueprint(
+      'output',
+      ElementType.OUTPUT,
+      1,
+      0,
+      'green',
+      'red',
+      '/static/components/output.png'
+    );
   }
 
   static createDefaultGateBlueprint() {
-    return new ElementBlueprint(ElementType.GATE, 2, 1, 'green', 'blue');
+    return new ElementBlueprint(
+      'and',
+      ElementType.GATE,
+      2,
+      1,
+      'green',
+      'blue',
+      '/static/components/and.png'
+    );
+  }
+
+  static createOrGateBlueprint() {
+    return new ElementBlueprint(
+      'or',
+      ElementType.GATE,
+      2,
+      1,
+      'green',
+      'blue',
+      '/static/components/or.png'
+    );
   }
 }
 
