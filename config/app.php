dae1cdc1fd15a76c05f8026fdb57da0f7f328e49
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-05-10 17:54:52 +0200

    board: add mongodb model

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Appstract\Opcache\OpcacheServiceProvider::class,
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
 
     ],
 
