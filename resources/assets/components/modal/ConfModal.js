c6144b390fdc57ff51c06fe366d83c8944246411
Arthur Lehuen <arthur@lehuen.net>
2017-05-11 14:44:03 +0200

    modal: confirmation modal first try

---
+++ resources/assets/components/modal/ConfModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function SelectableElement(props) {
+  return (
+    <div
+      className={classNames('modal', {
+        'is-active': props.isOpen
+      })}
+    >
+      <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">{props.title}</p>
+          <button onClick={props.onCancel} className="delete" />
+        </header>
+        <section className="modal-card-body">
+          <p>{props.content}</p>
+        </section>
+        <footer className="modal-card-foot">
+          <a className="button is-success" onClick={props.onApply}>
+            {props.success}
+          </a>
+          <a className="button" onClick={props.onCancel}>Cancel</a>
+        </footer>
+      </div>
+    </div>
+  );
+}
