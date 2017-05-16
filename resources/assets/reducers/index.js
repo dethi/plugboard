13371dcd5e00179f7b242c6c86ac0b3d11f0123c
Arthur Lehuen <arthur@lehuen.net>
2017-05-16 23:46:50 +0200

    app: store palette info in redux store

--- resources/assets/reducers/index.js
+++ resources/assets/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import palette from './palette';
 
 const plugboardReducers = combineReducers({
-  user
+  user,
+  palette
 });
 
 export default plugboardReducers;
