e8847a5212d000c901cba16ebdf02790cc5f36d8
Arthur Lehuen <arthur@lehuen.net>
2017-05-22 19:06:21 +0200

    modal: add onDisplay support

--- resources/assets/components/Board.js
+++ resources/assets/components/Board.js
@@ -15,62 +15,6 @@ class Board extends Component {
     this.state = {
       timerId: null
     };
-    // FIXME: Static data for tests
-    this.board1 = {
-      elements: {
-        '1': {
-          id: 1,
-          specName: 'INPUT',
-          pos: { x: 3, y: 1 },
-          rotate: 0,
-          input: {},
-          inputState: {},
-          output: { A: [[3, 'A']] }
-        },
-        '2': {
-          id: 2,
-          specName: 'OUTPUT',
-          pos: { x: 7, y: 1 },
-          rotate: 0,
-          input: { A: [3, 'B'] },
-          inputState: { A: 0 },
-          output: {}
-        },
-        '3': {
-          id: 3,
-          specName: 'GATE_NOT',
-          pos: { x: 5, y: 1 },
-          rotate: 0,
-          input: { A: [1, 'A'] },
-          inputState: { A: 0 },
-          output: { B: [[2, 'A']] }
-        }
-      },
-      specs: {
-        INPUT: {
-          name: 'INPUT',
-          input: [],
-          output: ['A'],
-          img: 'Input',
-          imgOn: 'InputOn'
-        },
-        OUTPUT: {
-          name: 'OUTPUT',
-          input: ['A'],
-          output: [],
-          img: 'Output',
-          imgOn: 'OutputOn'
-        },
-        GATE_NOT: {
-          name: 'GATE_NOT',
-          input: ['A'],
-          output: ['B'],
-          img: 'Not',
-          truthTable: [[0, 1], [1, 0]]
-        }
-      }
-    };
-    // END FIXME
   }
 
   componentDidMount() {
