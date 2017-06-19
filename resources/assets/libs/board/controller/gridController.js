c369a0c67c5b5a757017799bff2d511b3d5a964b
Arthur Lehuen <arthur@lehuen.net>
2017-06-19 23:48:45 +0200

    link: own pathfinding search

--- resources/assets/libs/board/controller/gridController.js
+++ resources/assets/libs/board/controller/gridController.js
@@ -1,7 +1,4 @@
-import PathFinding from 'pathfinding';
-
-import { GRID_SIZE } from '../constante';
-import Vector from '../../utils/vector';
+import myPathFinding from '../../utils/pathfinding';
 
 export default class GridController {
   constructor(sizeX, sizeY) {
@@ -10,13 +7,11 @@ export default class GridController {
 
     this.space = new Array(this.sizeX * this.sizeY).fill(null);
 
-    this.pfGrid = new PathFinding.Grid(this.sizeX, this.sizeY);
+    this.myPathFinding = new myPathFinding(this);
   }
 
   setElement(el) {
     this.set(el.pos, el.id);
-
-    this.pfGrid.setWalkableAt(el.pos.x, el.pos.y, false);
   }
 
   canMove(el, newPos) {
@@ -25,29 +20,30 @@ export default class GridController {
 
   moveElement(el, oldPos) {
     this.set(el.pos, el.id);
-    this.pfGrid.setWalkableAt(el.pos.x, el.pos.y, false);
 
     this.set(oldPos, null);
-    this.pfGrid.setWalkableAt(oldPos.x, oldPos.y, true);
   }
 
   freeEl(el) {
     this.set(el.pos, null);
-    this.pfGrid.setWalkableAt(el.pos.x, el.pos.y, true);
   }
 
   getPath(posA, posB) {
-    const bestFirstFinder = new PathFinding.BestFirstFinder();
+    const myPath = this.myPathFinding.findPath(posA, posB);
 
-    const path = bestFirstFinder.findPath(
-      posA.x,
-      posA.y,
-      posB.x,
-      posB.y,
-      this.pfGrid.clone()
-    );
+    // Need to compress Path
+    return myPath;
+  }
 
-    return PathFinding.Util.compressPath(path);
+  isInBound(pos) {
+    if (pos.x < 0 || pos.x >= this.sizeX || pos.y < 0 || pos.y >= this.sizeY) {
+      return false;
+    }
+    return true;
+  }
+
+  isFree(pos) {
+    return this.isInBound(pos) && !this.get(pos);
   }
 
   get(pos) {
