63a8ab1a89473edf9e899e092b0c4e6d4bad848b
Antoine C <antoine.cormerais@epita.fr>
2017-06-20 12:26:09 +0200

    js: add better "disposition interactive" for every component relative to the objectifs.

--- resources/assets/components/objectif/ItemInBoard.js
+++ resources/assets/components/objectif/ItemInBoard.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 export default class ItemList extends Component {
   render() {
     return (
-      <div className="column is-2 on-canvas">
-        <div className="box">
-          <figure className="image is-128x128">
+      <div className="card on-canvas board-objectif">
+        <div className="card-image">
+          <figure className="image is-square">
             <img
-              src="http://bulma.io/images/placeholders/128x128.png"
+              src="http://bulma.io/images/placeholders/256x256.png"
               alt="screenshot"
             />
           </figure>
-          <div className="has-text-centered">
-            Challenge 1
+        </div>
+        <div className="card-content">
+          <div className="content has-text-centered">
+            <h1 className="title is-4 is-spaced">
+              Challenge 1
+            </h1>
           </div>
-
         </div>
       </div>
     );
