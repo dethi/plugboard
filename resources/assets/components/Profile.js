e36fff2acd7f3b699579ead0bfa3f6caf9556759
Antoine C <antoine.cormerais@epita.fr>
2017-06-21 14:18:39 +0200

    js: add dispo interactive for profile

--- resources/assets/components/Profile.js
+++ resources/assets/components/Profile.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Profile extends Component {
+import defaultUserProfilPicture
+  from '../../../public/static/default-user-profile.png';
+
+class Profile extends Component {
   render() {
     return (
-      <div className="column">
-        <h2>Name</h2>
-        <h3>Level 15</h3>
-        <progress value="15" max="100">15%</progress>
-        <Tabs>
-          <TabList>
-            <Tab>Informations</Tab>
-            <Tab>Achievements</Tab>
-          </TabList>
-          <TabPanel />
-          <TabPanel />
-        </Tabs>
+      <div>
+        <section className="hero is-primary is-bold">
+          <div className="hero-body">
+            <div className="profile container">
+              <div className="has-text-centered">
+                <figure className="image image-is-centered is-128x128">
+                  <img
+                    src={defaultUserProfilPicture}
+                    className="is-centered"
+                    alt="profile"
+                  />
+                </figure>
+                <h1 className="title">
+                  {this.props.user ? this.props.user.name : 'Name'}
+                </h1>
+                <h2 className="subtitle">
+                  {this.props.user ? this.props.user.email : 'Email'}
+                </h2>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="columns">
+          <div className="column is-one-quarter" />
+          <div className="column is-half">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Donnée</th>
+                  <th>Valeur</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Circuit créé</th>
+                  <th>35</th>
+                </tr>
+                <tr>
+                  <th>Etoile débloquée</th>
+                  <th>15</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="column is-one-third" />
+        </div>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+Profile.PropTypes = {
+  user: PropTypes.object
+};
+
+export default connect(mapStateToProps)(Profile);
