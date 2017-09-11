bde778bf2327d9d8e2316fe8f80c197ff75a8e2b
Antoine C <antoine.cormerais@epita.fr>
2017-09-11 21:42:35 +0200

    add navbar and offset to the list of objectifs

--- resources/assets/components/objectif/ListObjectif.js
+++ resources/assets/components/objectif/ListObjectif.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import ItemList from './ItemList';
+import NavBarAccueil from '../home/NavBarAccueil';
 
 export default class ListObjectif extends Component {
   render() {
     return (
-      <div className="conteneur">
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-        <ItemList />
-      </div>
+      <div id="landing">
+        <section className="hero is-fullheight is-dark">
+          <NavBarAccueil />
+          <div className="columns is-mobile">
+            <div className="column is-half is-offset-one-quarter">
+              <div className="conteneur">
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
+              </div>
+            </div>
+        </div>
+      </section>
+  </div>  
     );
   }
 }
