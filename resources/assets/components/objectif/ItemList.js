2ec212e7272010fd4f92b990ecb0424e1d709a7a
Antoine C <antoine.cormerais@epita.fr>
2017-09-12 20:39:05 +0200

    complete dispo interactive of list objectif

--- resources/assets/components/objectif/ItemList.js
+++ resources/assets/components/objectif/ItemList.js
@@ -3,22 +3,42 @@ import { NavLink } from 'react-router-dom';
 
 export default class ItemList extends Component {
   render() {
-    return (
-      <div className=" element">
-        <NavLink to="/app">
-
+    console.log(this.props.locked);
+    if (!this.props.locked) {
+      return (
+        <div className=" element">
           <div className="card">
             <div className="card-image">
               <figure className="image is-square">
-                <img
-                  src="http://bulma.io/images/placeholders/256x256.png"
-                  alt="screenshot"
-                />
+                <img alt="Locked" src="/static/lock.png" />
               </figure>
             </div>
             <div className="card-content">
 
-            {/*
+              <div className="content has-text-centered">
+                Locked
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className=" element">
+          <NavLink to="/app">
+
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-square">
+                  <img
+                    src="http://bulma.io/images/placeholders/256x256.png"
+                    alt="screenshot"
+                  />
+                </figure>
+              </div>
+              <div className="card-content">
+
+                {/*
               <div className="media">
                 <div className="media-right">
                   <div className="icon">
@@ -29,13 +49,14 @@ export default class ItemList extends Component {
                 </div>
               </div>
               */}
-              <div className="content has-text-centered">
+                <div className="content has-text-centered">
                   Challenge 1
+                </div>
               </div>
             </div>
-          </div>
-        </NavLink>
-      </div>
-    );
+          </NavLink>
+        </div>
+      );
+    }
   }
 }
