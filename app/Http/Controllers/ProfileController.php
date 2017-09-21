3f7db6406150ee6310a43597ae335b8936948de9
Antoine C <antoine.cormerais@epita.fr>
2017-09-21 18:42:22 +0200

    add backend to update password show error or success message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,22 +10,30 @@ class ProfileController extends Controller
 {
     public function update(Request $request)
     {
-
-
-        $input = $request->only('name', 'email', 'old_password', 'new_password', 'wants_autosav');
-
-        if (isset($input['new_password'])) {
-             $this->validate($request, [
-            'old_password' => 'required|min:6',
-            'new_password' => 'required|min:6|confirmed'
-        ]);
-        }
         $this->validate($request, [
             'name' => 'required|max:255',
             'wants_autosav' => 'required|boolean'
         ]);
 
+        $input = $request->only('name', 'email', 'old_password', 'new_password', 'wants_autosav');
+
         $user = Auth::user();
+        if (isset($input['new_password'])) {
+            $this->validate($request, [
+                'old_password' => 'required|min:6',
+                'new_password' => 'required|min:6|confirmed'
+            ]);
+            if (Hash::check($input['old_password'], $user->password)) {
+                $user->password = bcrypt($input['new_password']);
+            }
+            else {
+                return response([
+                    'code' => 'failed_old_password',
+                    'status' => 'Wrong password'
+                ], 401);
+            }
+        }
+
         $user->name = $input['name'];
         $user->wants_autosav = $input['wants_autosav'];
         $user->save();
