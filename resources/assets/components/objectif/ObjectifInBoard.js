fbca06d3d5d89266423e7344acd0d230a76c0fb8
Antoine C <antoine.cormerais@epita.fr>
2017-10-04 12:45:14 +0200

    objectif: modify box in board and update position due to the new grid

--- resources/assets/components/objectif/ObjectifInBoard.js
+++ resources/assets/components/objectif/ObjectifInBoard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class ObjectifInBoard extends Component {
   render() {
     const { currentObjectif } = this.props.objectif;
     return (
-      <div className="card on-canvas board-objectif">
+      <div className="box on-canvas board-objectif">
         {/* Maybe use a box ? */}
         {/*
         <div className="card-image">
@@ -17,19 +18,28 @@ class ObjectifInBoard extends Component {
             />
           </figure>
         </div>*/}
-        <div className="card-content">
-          <div className="content has-text-centered">
-            <h1 className="title is-spaced">
-              {currentObjectif.title}
-            </h1>
+        <h1 className="title has-text-centered is-spaced">
+          {currentObjectif.title}
+        </h1>
+
+        <nav className="level">
+          <div className="level-item has-text-centered">
+            <span className="icon is-medium has-text-info">
+              <NavLink to="/objectif">
+                <i className="fa fa-list" />
+              </NavLink>
+            </span>
+          </div><div className="level-item has-text-centered">
             <span className="icon is-medium has-text-info">
               <i className="fa fa-play" />
             </span>
+          </div>
+          <div className="level-item has-text-centered">
             <span className="icon is-medium has-text-info">
               <i className="fa fa-info-circle" />
             </span>
           </div>
-        </div>
+        </nav>
       </div>
     );
   }
