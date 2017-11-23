38c86b0766b4a62c53f191da57e7f6ccb754eb94
Antoine C <antoine.cormerais@epita.fr>
2017-11-23 17:50:32 +0100

    objectif: fix wrong variable name and round the score

--- resources/assets/libs/utils/objectif.js
+++ resources/assets/libs/utils/objectif.js
@@ -19,20 +19,20 @@ const computeScore = (objectifStartedAt, nbElementRemoved) => {
   let scores = [];
   let total = 0;
 
-  const timeSpent = (Date.now() - objectifStartedAt) / 1000;
+  const timeSpent = Math.round((Date.now() - objectifStartedAt) / 1000);
   let el1 = {
     name: 'Time spent',
     value: timeSpent,
-    point: 10 - timeSpent / 10 > 0 ? 10 - timeSpent / 10 : 0
+    score: Math.round(10 - timeSpent / 10 > 0 ? 10 - timeSpent / 10 : 0)
   };
-  total = el1.point;
+  total = el1.score;
   scores.push(el1);
   let el2 = {
     name: 'Element removed',
     value: nbElementRemoved,
-    point: 10 - nbElementRemoved > 0 ? 10 - nbElementRemoved : 0
+    score: 10 - nbElementRemoved > 0 ? 10 - nbElementRemoved : 0
   };
-  total += el2.point;
+  total += el2.score;
   scores.push(el2);
 
   return { scores, total };
