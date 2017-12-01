bc1e7ad0569358189ec97f7ab4d97781c65d0792
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-12-01 05:26:20 +0100

    fix: flip renderOnAddRemove to fix animation not rendering when output state doesn't change

--- resources/assets/libs/board/view/boardView.js
+++ resources/assets/libs/board/view/boardView.js
@@ -30,7 +30,7 @@ export default class BoardView {
       selection: false,
       height: this.gridHeight,
       width: this.gridWidth,
-      renderOnAddRemove: false
+      renderOnAddRemove: true
     });
 
     this.leftMin = this.gridSize;
