0fa5a4f0276b0540db8592a48a9a3a41df62697b
Antoine C <antoine.cormerais@epita.fr>
2017-12-01 14:22:55 +0100

    feature: redesign board and component tabs in profile

--- resources/assets/components/profile/Element.js
+++ resources/assets/components/profile/Element.js
@@ -5,6 +5,33 @@ import { NavLink } from 'react-router-dom';
 export default function Element(props) {
   return (
     <div className="box">
+      <article className="media">
+        <div className="media-content">
+          <figure className="image">
+            <img src={props.img} alt="Board Preview" />
+          </figure>
+          <div className="content has-text-centered">
+            <strong className="title">{props.title}</strong><br />
+            <NavLink
+              className="has-text-center button is-medium is-primary has-margin"
+              to="/app"
+              onClick={props.onClick}
+            >
+              Edit
+            </NavLink>
+            <a
+              className="has-text-center button is-medium is-danger has-margin"
+            >
+              Delete
+            </a>
+
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+  /*
+    <div className="box">
       <div className="has-text-centered">
         <strong className="title">{props.title}</strong>
       </div>
@@ -40,6 +67,5 @@ export default function Element(props) {
           </div>
         </div>
       </article>
-    </div>
-  );
+    </div>*/
 }
